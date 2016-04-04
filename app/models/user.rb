e3f3d0a115828f0785d309122dd8e18c7aef219f
Arturo Perez <VrturoPerez@gmail.com>
2016-04-03 19:31:55 -0700

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_many :bookings
   has_many :bookings, foreign_key: "guest_id"
   has_many :booked_rooms, through: :bookings, source: :room
+  has_many :booked_hotels, through: :booked_rooms, source: :hotel
 end
